0ccad23607ba0da041ff2c95cf190c6283c236a6
mohithworks <mohithworks2002@gmail.com>
2022-10-14 00:07:02 +0530

    first commit

---
+++ terraform/main.tf
@@ -0,0 +1,47 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      Version = "~>3.27"
+    }
+  }
+
+  required_version = ">=0.14.9"
+
+   backend "s3" {
+       bucket = "vinayworks"
+       key    = "arn:aws:kms:ap-south-1:211544326561:alias/aws/s3"
+       region = "ap-south-1"
+   }
+}
+
+provider "aws" {
+  version = "~>3.0"
+  region  = "ap-south-1"
+}
+
+resource "aws_s3_bucket" "s3Bucket" {
+     bucket = "vinayworks"
+     acl       = "public-read"
+
+     policy  = <<EOF
+{
+     "id" : "MakePublic",
+   "version" : "2012-10-17",
+   "statement" : [
+      {
+         "action" : [
+             "s3:GetObject"
+          ],
+         "effect" : "Allow",
+         "resource" : "arn:aws:s3:::vinayworks/*",
+         "principal" : "*"
+      }
+    ]
+  }
+EOF
+
+   website {
+       index_document = "index.html"
+   }
+}
