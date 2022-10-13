d6c7079e02e12132f5c81804bf02afefb8ed24c3
mohithworks <mohithworks2002@gmail.com>
2022-10-14 01:11:18 +0530

    first commit 4

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,6 +2,7 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
+      version = "4.34.0"
     }
   }
 
@@ -15,6 +16,7 @@ terraform {
 }
 
 provider "aws" {
+  version = "4.34.0"
   region  = "ap-south-1"
 }
 
