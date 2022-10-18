2c6e32c69131c2e785418ba3022ae9ff8d5019e3
mohithworks <mohithworks2002@gmail.com>
2022-10-19 01:39:57 +0530

    test 7

--- terraform/install_website.sh
+++ terraform/install_website.sh
@@ -5,7 +5,7 @@ yum install -y httpd
 cd /var/www/html
 wget https://github.com/go-trav/go-trav/archive/refs/heads/main.zip
 unzip main.zip
-cp -r go-trav-main/* /var/www/html/
+cp -r go-trav-main/go-trav-main/* /var/www/html/
 rm -rf go-trav-main main.zip
 systemctl enable httpd 
 systemctl start httpd
