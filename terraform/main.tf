8f708538621f6107f7763a523c28339b6ddc4e24
mohithworks <mohithworks2002@gmail.com>
2022-10-20 01:20:53 +0530

    Modified sh file to s3

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,27 +1,15 @@
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/terraform-provider-aws"
-      version = "4.34.0"
-    }
-  }
-
-  required_version = ">=0.14.9"
-
-   backend "s3" {
-       bucket = "vinayworks"
-       key    = "arn:aws:kms:ap-south-1:211544326561:alias/aws/s3"
-       region = "ap-south-1"
-   }
-}
-
 provider "aws" {
-  version = "4.34.0"
   region  = "ap-south-1"
 }
-on:
-  push:
-    branches: [ "main" ]
+
+terraform {
+  backend "s3" {
+      bucket = "vinayworks"
+      key    = "build/terraform.tfstate"
+      region = "ap-south-1"
+  }
+}
+
 resource "aws_s3_bucket" "s3Bucket" {
      bucket = "vinayworks"
      acl       = "public-read"
